6af89fd1c62b9d43910277d5e1ee07ec221c6c82
Dương <leduong.w01@gmail.com>
2023-02-05 18:26:05 +0700

    tạo icon

---
+++ app/src/main/java/com/duong/ideatimetable/CreateNoteScreen.kt
@@ -0,0 +1,11 @@
+package com.duong.ideatimetable
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateNoteScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_note_screen)
+    }
+}
